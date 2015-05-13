b7a7c019a5ba9c9aa7f714148192f46943d766ae
amcaplan <ariel.caplan@vitals.com>
2015-05-13 23:19:47 +0300

    Get most tests passing by defining class methods not singleton methods

--- lib/persistent_open_struct.rb
+++ lib/persistent_open_struct.rb
@@ -3,5 +3,12 @@ require 'ostruct'
 class PersistentOpenStruct < OpenStruct
   VERSION = "0.0.1"
 
-  
+  def new_ostruct_member(name)
+    name = name.to_sym
+    unless respond_to?(name)
+      self.class.send(:define_method, name) { @table[name] }
+      self.class.send(:define_method, "#{name}=") { |x| modifiable[name] = x }
+    end
+    name
+  end
 end
